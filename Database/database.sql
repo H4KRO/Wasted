4c130fbc1a02247db2c086b871e858fe8c75ad3a
H4KRO <32486404+H4KRO@users.noreply.github.com>
2020-02-02 17:58:41 +0100

    Update database.sql

--- Database/database.sql
+++ Database/database.sql
@@ -1,1 +1,40 @@
-#Will contain the sql of the DATABASE.
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- HÃ´te : 127.0.0.1:3306
+-- GÃ©nÃ©rÃ© le :  Dim 02 fÃ©v. 2020 Ã  16:58
+-- Version du serveur :  5.7.26
+-- Version de PHP :  7.3.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de donnÃ©es :  `wasted`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `test`
+--
+
+DROP TABLE IF EXISTS `test`;
+CREATE TABLE IF NOT EXISTS `test` (
+  `id` int(11) NOT NULL,
+  `nombre` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
