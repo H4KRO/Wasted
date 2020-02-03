82f18362b4eabcf3c8fb63769cbe49f5db68b641
Degrave Julien <juliendegravesky@gmail.com>
2020-02-03 13:31:55 +0100

    Update content.php

--- template/content.php
+++ template/content.php
@@ -1,6 +1,7 @@
 <?php
 $page = 'main';
-if ( isset( $_GET['page'] ) && $_GET['page'] != '' && file_exists( $TEMPLATEPATH . "pages/" . $_GET['page'] . '.php' ) ){
+if ( isset( $_GET['page'] ) && $_GET['page'] != '' && file_exists( $TEMPLATEPATH . "pages/" . $_GET['page'] . '.php' ) )
+{
 	$page = $_GET['page'];
 }
 require_once($TEMPLATEPATH . "pages/" . $page . ".php");
