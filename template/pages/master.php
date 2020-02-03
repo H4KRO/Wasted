1bc4c6be91fa137d9e5a8afa007f0c3f324cee15
Degrave Julien <juliendegravesky@gmail.com>
2020-02-03 18:04:22 +0100

    Avancing HTML/CSS, template

---
+++ template/pages/master.php
@@ -0,0 +1,14 @@
+<section class ="choice">
+    <form methode="post" class = "choice_peudo">
+        Choisir un pseudo : <input type="text" maxlength="10" name="pseudo">
+    </form>
+</section>
+
+<section class="choice">
+    <a href="#" >$LIEN_INC</a>
+    <a href="#">Copier ce lien</a>
+</section>
+
+<section class="choice">
+    <p id="salon">Salon</p>
+</section>
