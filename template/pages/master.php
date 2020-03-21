e45900bf17d2cb1763b9a0e3ae5fa6ca39c46550
JulienDegrave <60577789+JulienDegrave@users.noreply.github.com>
2020-03-21 19:53:09 +0100

    Add Start button

---
+++ template/pages/master.php
@@ -0,0 +1,11 @@
+<section id="master_top"class ="section">
+    <form action="" methode="post" id="pseudo">
+        <p><strong>Pseudo</strong></p>
+        <input id="master_input"type="text" maxlength="15" name="pseudo">
+        <!-- <input id="ok" type="submit" value="Ok"> -->
+    </form>
+</section>
+
+<section id="master_bot" class="section">
+    <a class="p_section" href="" >Start</a>
+</section>
