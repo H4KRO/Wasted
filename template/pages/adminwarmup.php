65019e4fdc961ee3d70299366007ad5bc383edec
H4KRO <32486404+H4KRO@users.noreply.github.com>
2020-03-21 20:29:03 +0100

    Update adminwarmup.php

--- template/pages/adminwarmup.php
+++ template/pages/adminwarmup.php
@@ -1,12 +1,8 @@
 <section id="master_top" class ="section">
     <form action="" methode="post" id="pseudo">
         <p>Ajoute des gages</p>
-<<<<<<< HEAD
-        <input id="master_input" type="text" maxlength="50" name="gages">
-=======
         <input id="master_input" type="text" maxlength="15" name="gages">
         <input type="hidden" name="page" value="adminwarmup">
->>>>>>> bd08943387447e513f6fcae15375c0b8e4134e4c
         <input class="ok" type="submit" value="Ok">
     </form>
 </section>
