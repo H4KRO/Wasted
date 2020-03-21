9ba97b53c75ad443509c28d63b38613a2b2e058a
H4KRO <32486404+H4KRO@users.noreply.github.com>
2020-03-21 19:51:35 +0100

    Initialisation des routes

---
+++ template/pages/adminwarmup.php
@@ -0,0 +1,6 @@
+<div id="main_top" class ="section">
+      <p class="p_section">Ajouter des gages & <a href="?page=admingame">commencer</a></p>
+</div>
+<div id="main_bot" class ="section">
+      <p class="p_section">Options de partage</p>
+</div>
