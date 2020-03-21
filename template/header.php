0bd254b823bbef5dc913a2f63ae56d0de5d878b6
JulienDegrave <60577789+JulienDegrave@users.noreply.github.com>
2020-03-21 13:37:34 +0100

    MAJ font of title and section
    Voila, c'est tout pour ce matin, tu me diras ce que tu en pense

--- template/header.php
+++ template/header.php
@@ -2,6 +2,7 @@
 <html>
 <head>
   <title>Wasted!</title>
+  <link href="https://fonts.googleapis.com/css?family=Bungee+Shade|Luckiest+Guy&display=swap" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="<?php echo $STYLEPATH; ?>style.css">
 </head>
 <body>
