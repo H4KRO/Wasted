d5cc8d4faadfccacb6ebff864179e153d10930ab
JulienDegrave <60577789+JulienDegrave@users.noreply.github.com>
2020-03-21 20:01:28 +0100

    hihi

--- template/pages/create.php
+++ template/pages/create.php
@@ -1,6 +1,11 @@
-<div id="main_top" class ="section">
-      <p class="p_section">Entrer pseudo</p>
-</div>
-<div id="main_bot" class ="section">
-      <a href="?page=adminwarmup"><p class="p_section">Commencer</p></a>
-</div>
+<section id="master_top"class ="section">
+    <form action="" methode="post" id="pseudo">
+        <p><strong>Pseudo</strong></p>
+        <input id="master_input"type="text" maxlength="15" name="pseudo">
+        <!-- <input id="ok" type="submit" value="Ok"> -->
+    </form>
+</section>
+
+<section id="master_bot" class="section">
+    <a class="p_section" href="?page=adminwarmup" >Start</a>
+</section>
